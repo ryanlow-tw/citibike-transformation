4766ff0a5452b72f603447b9d3a824d1525d7e1e
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-26 15:11:28 +0800

    add word count functionality

--- src/main/scala/thoughtworks/wordcount/WordCountUtils.scala
+++ src/main/scala/thoughtworks/wordcount/WordCountUtils.scala
@@ -1,16 +1,26 @@
 package thoughtworks.wordcount
-
 import org.apache.spark.sql.{Dataset, SparkSession}
 
 object WordCountUtils {
   implicit class StringDataset(val dataSet: Dataset[String]) {
-    def splitWords(spark: SparkSession) = {
-      dataSet
-    }
-
-    def countByWord(spark: SparkSession) = {
+    def splitWords(spark: SparkSession): Dataset[String] = {
       import spark.implicits._
-      dataSet.as[String]
+
+      // Split on all non-words that are also not apostrophe
+      val regex: String = "(?!')\\W+"
+
+      dataSet.flatMap(_.split(regex)).map(_.trim.toLowerCase).filter(_.nonEmpty)
+    }
+    def countByWord(spark: SparkSession): Dataset[(String, Int)] = {
+      import spark.implicits._
+      dataSet.map((_, 1))
+        .groupByKey(_._1)
+        .reduceGroups((a, b) => (a._1, a._2 + b._2))
+        .map(_._2)
+        .orderBy("_1")
+        .withColumnRenamed("_1","word")
+        .withColumnRenamed("_2","count")
+        .as[(String, Int)]
     }
   }
 }
