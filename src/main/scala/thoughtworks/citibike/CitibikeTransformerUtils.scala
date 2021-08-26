225e5425a89e2ad15eaedfaacd56f5a930100c2f
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-26 15:11:41 +0800

    add distance calculator

--- src/main/scala/thoughtworks/citibike/CitibikeTransformerUtils.scala
+++ src/main/scala/thoughtworks/citibike/CitibikeTransformerUtils.scala
@@ -1,6 +1,10 @@
 package thoughtworks.citibike
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.col
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import scala.math._
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.udf
 
 object CitibikeTransformerUtils {
   private final val MetersPerFoot = 0.3048
@@ -11,8 +15,26 @@ object CitibikeTransformerUtils {
 
   implicit class StringDataset(val dataSet: Dataset[Row]) {
 
-    def computeDistances(spark: SparkSession) = {
-      dataSet
+    def computeDistances(spark: SparkSession): DataFrame = {
+
+      val UDF: UserDefinedFunction = udf((startLat: Double, startLong: Double,
+                                              endLat: Double, endLong: Double) => {
+        val startLatRad = startLat * Pi / 180
+        val endLatRad = endLat * Pi / 180
+        val latDelta = endLatRad - startLatRad
+        val lonDelta = (endLong - startLong) * Pi / 180
+
+        // refer to haversine formula
+        val a = sin(latDelta/2) * sin(latDelta/2) + cos(startLatRad) * cos(endLatRad) * sin(lonDelta/2) * sin(lonDelta/2)
+        val c = 2 * atan2(sqrt(a), sqrt(1-a))
+        (EarthRadiusInM * c / MetersPerMile).formatted("%.2f").toDouble
+      })
+
+      dataSet.withColumn("distance",
+          UDF(col("start_station_latitude"),
+            col("start_station_longitude"),
+            col("end_station_latitude"),
+            col("end_station_longitude")))
     }
   }
 }
--- src/test/scala/thoughtworks/citibike/CitibikeTransformerTest.scala
+++ src/test/scala/thoughtworks/citibike/CitibikeTransformerTest.scala
@@ -48,7 +48,7 @@ class CitibikeTransformerTest extends DefaultFeatureSpecWithSpark {
         .fields.map(fieldToFieldIgnoringNullable))
     }
 
-    ignore("Citibike Advanced Acceptance Test") {
+    scenario("Citibike Advanced Acceptance Test") {
       val rootDirectory = Files.createTempDirectory(this.getClass.getName + "Citibike")
       val ingestedDir = rootDirectory.resolve("ingest")
       val transformedDir = rootDirectory.resolve("transform")
@@ -73,7 +73,7 @@ class CitibikeTransformerTest extends DefaultFeatureSpecWithSpark {
       val expectedData = Array(
         Row(328, "2017-07-01 00:00:08", "2017-07-01 00:05:37", 3242, "Schermerhorn St & Court St", 40.69102925677968, -73.99183362722397, 3397, "Court St & Nelson St", 40.6763947, -73.99869893, 27937, "Subscriber", 1984, 2, 1.07),
         Row(1496, "2017-07-01 00:00:18", "2017-07-01 00:25:15", 3233, "E 48 St & 5 Ave", 40.75724567911726, -73.97805914282799, 546, "E 30 St & Park Ave S", 40.74444921, -73.98303529, 15933, "Customer", 1971, 1, 0.92),
-        Row(1067, "2017-07-01 00:16:31", "2017-07-01 00:34:19", 448, "W 37 St & 10 Ave", 40.75660359, -73.9979009, 487, "E 20 St & FDR Drive", 40.73314259, -73.97573881, 27084, "Subscriber", 1990, 2.0, 1.99)
+        Row(1067, "2017-07-01 00:16:31", "2017-07-01 00:34:19", 448, "W 37 St & 10 Ave", 40.75660359, -73.9979009, 487, "E 20 St & FDR Drive", 40.73314259, -73.97573881, 27084, "Subscriber", 1990, 2, 1.99)
       )
 
       transformedDF.schema("distance") should be(StructField("distance", DoubleType, nullable = true))
